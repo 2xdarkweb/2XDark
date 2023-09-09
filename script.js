228472ec1270d0b06292fcc3d737d205d7bbf7a1
2xdark <113830657+2xdarkweb@users.noreply.github.com>
2023-09-09 04:12:18 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburge =document.querySelector('.hamburge');
-const navbar = document.querySelector('ul')
-
-hamburge.addEventListener('click',()=>{
-    navbar.classList.toggle('slide');
-});
